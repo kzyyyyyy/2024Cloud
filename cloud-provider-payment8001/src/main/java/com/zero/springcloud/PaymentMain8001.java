3aaac5ecb4b396034dbdce87d535dc516b573974
kzyyyyy <a87977195@qq.com>
2024-06-07 11:01:11 +0800

    payment8001 pom

---
+++ cloud-provider-payment8001/src/main/java/com/zero/springcloud/PaymentMain8001.java
@@ -0,0 +1,9 @@
+package com.zero.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
